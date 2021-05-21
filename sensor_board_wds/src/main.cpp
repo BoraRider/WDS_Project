019398c1345c298ff89d10b36f00267f37bbc1c4
Maciej Osak <maciek3092@gmail.com>
2021-05-21 14:32:52 +0200

    wysyla przetworzone katy

--- sensor_board_wds/src/main.cpp
+++ sensor_board_wds/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Wire.h"
 #include <stdlib.h>
 
+
 #define POLYNOMIAL_8 0x2F // wielomian 8 bitowy (używany w przemyśle motoryzacyjnym)
 const int MPU_ADDR = 0x68; // adres I2C dla MPU-6050
 
@@ -9,6 +10,10 @@ typedef struct data{
     int16_t gyro_x;
     int16_t gyro_y;
     int16_t gyro_z;
+    int16_t acc_x;
+    int16_t acc_y;
+    int16_t acc_z;
+    int16_t temp;
     uint16_t adcValue[5];
 };
 
@@ -19,6 +24,7 @@ int loop_counter=0, sum_counter=0;
 uint8_t str_len=0;
 byte arr[128];
 byte sum_crc;
+long gyro_x_cal, gyro_y_cal, gyro_z_cal;
 
 // tworzenie sumy kontrolnej
 byte CRC8_SingleByte(byte CRC_prev, byte Data)
@@ -80,6 +86,21 @@ void read_adc()
   sensors.adcValue[4] = analogRead(A7);
 }
 
+void read_imu_data()
+{
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B); 
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, 7*2, true);
+    sensors.acc_x = Wire.read()<<8 | Wire.read();
+    sensors.acc_y = Wire.read()<<8 | Wire.read();
+    sensors.acc_z = Wire.read()<<8 | Wire.read();
+    sensors.temp = Wire.read()<<8 | Wire.read();
+
+    sensors.gyro_x = Wire.read()<<8 | Wire.read();
+    sensors.gyro_y = Wire.read()<<8 | Wire.read();
+    sensors.gyro_z = Wire.read()<<8 | Wire.read();
+}
 // konfiguracja I2C oraz UART
 void setup() {
   Serial.begin(9600);
@@ -89,42 +110,69 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
 
+
+  for (int cal_int = 0; cal_int < 1000 ; cal_int ++){                  
+    read_imu_data(); 
+    //Add the gyro x offset to the gyro_x_cal variable                                            
+    gyro_x_cal += sensors.gyro_x;
+    //Add the gyro y offset to the gyro_y_cal variable                                              
+    gyro_y_cal += sensors.gyro_y; 
+    //Add the gyro z offset to the gyro_z_cal variable                                             
+    gyro_z_cal += sensors.gyro_z; 
+    //Delay 3us to have 250Hz for-loop                                             
+    delay(3);                                                          
+  }
+  gyro_x_cal /= 1000;                                                 
+  gyro_y_cal /= 1000;                                                 
+  gyro_z_cal /= 1000;
+
 }
-void loop(){
 
-  read_adc();
+void loop()
+{
 
-  // tworzenie sumy 3 pomiarów
   if(loop_counter%6==0)
-  {  
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(0x3B); 
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_ADDR, 7*2, true);
-    sum_x += Wire.read()<<8 | Wire.read();
-    sum_y += Wire.read()<<8 | Wire.read();
-    sum_z += Wire.read()<<8 | Wire.read();
-    sum_counter++;
-  }
-  
-  // wyznaczanie średniej z 3 pomiarów
-  if(sum_counter == 2)
   {
-    sum_counter = 0;
-    sensors.gyro_x = sum_x / 3;
-    sensors.gyro_y = sum_y / 3;
-    sensors.gyro_z = sum_z / 3;
-    sum_x = 0;
-    sum_y = 0;
-    sum_z = 0;
+    read_imu_data();
+    sensors.gyro_x -= gyro_x_cal;                                                 
+    sensors.gyro_y -= gyro_y_cal;                                                 
+    sensors.gyro_z -= gyro_z_cal;
+
+    sensors.gyro_x /= 131;                                                 
+    sensors.gyro_y /= 131;                                                 
+    sensors.gyro_z /= 131;
+
+    sum_x += sensors.gyro_x;
+    sum_y += sensors.gyro_y;
+    sum_z += sensors.gyro_z;
   }
-  
-  
   
   if(loop_counter%18==0)
   {
+    read_adc();
+
+    sensors.gyro_x = sum_x;
+    sensors.gyro_y = sum_y;
+    sensors.gyro_z = sum_z;
+    sensors.gyro_x *= 0.064;
+    sensors.gyro_y *= 0.064;
+    sensors.gyro_z *= 0.064;
+
+    if(sensors.gyro_x>360 || sensors.gyro_x<-360){
+      sensors.gyro_x=0;
+      sum_x=0;
+    }
+    if(sensors.gyro_y>360 || sensors.gyro_y<(-360)){
+      sensors.gyro_y=0;
+      sum_y=0;
+    }
+    if(sensors.gyro_z>360 || sensors.gyro_z<-360){
+      sensors.gyro_z=0;
+      sum_z=0;
+    }
+
     // Zapisywanie wszystkich danych do jednego ciagu znaków
-    sprintf(str, "%4d%4d%4d%4d%4d%6d%6d%6d", sensors.adcValue[0], sensors.adcValue[1], sensors.adcValue[2], sensors.adcValue[3], sensors.adcValue[4], sensors.gyro_x, sensors.gyro_y, sensors.gyro_z);
+    sprintf(str, "%4d%4d%4d%4d%4d%4d%4d%4d", sensors.adcValue[0], sensors.adcValue[1], sensors.adcValue[2], sensors.adcValue[3], sensors.adcValue[4], sensors.gyro_x, sensors.gyro_y, sensors.gyro_z);
     
     // zamiana ciągu znaków na ciąg bajtów
     string2ByteArray(str, arr);
@@ -139,6 +187,6 @@ void loop(){
   }
 
   loop_counter++;
-  delay(1);
+  delay(10);
   
 }
